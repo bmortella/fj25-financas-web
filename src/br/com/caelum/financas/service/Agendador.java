d4609eb1a5dec72ca8b9ee96404a2aeed535685b
Bruno Mortella <bmortella@gmail.com>
2015-10-04 22:21:48 -0300

    Trabalhando com TimerService

--- src/br/com/caelum/financas/service/Agendador.java
+++ src/br/com/caelum/financas/service/Agendador.java
@@ -2,12 +2,24 @@ package br.com.caelum.financas.service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.ejb.Schedule;
+import javax.ejb.ScheduleExpression;
+import javax.ejb.Startup;
 import javax.ejb.Stateless;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
 
+@Startup
 @Stateless
 public class Agendador {
 
 	private static int totalCriado;
+	
+	@Resource
+	private TimerService timerService;
 
 	public void executa() {
 		System.out.printf("%d instancias criadas %n", totalCriado);
@@ -30,5 +42,31 @@ public class Agendador {
 	void preDestruicao(){
 		System.out.println("destruindo agendador");
 	}
+	
+	@Schedule(hour="9", minute="0", second="0", dayOfWeek="Mon", persistent=false)
+	public void enviaEmailSemanalmente(){
+	    System.out.println("Enviando email semanal..");	
+	}
+	
+	@Timeout
+	public void verificacaoPeriodicaSeHaNovasContas(Timer timer){
+		System.out.println(timer.getInfo());
+	}
+	
+	public void agenda(String expressaoMinutos, String expressaoSegundos){
+		ScheduleExpression expression = new ScheduleExpression();
+		expression.hour("*");
+		expression.minute(expressaoMinutos);
+		expression.second(expressaoSegundos);
+		
+		TimerConfig config = new TimerConfig();
+		config.setInfo(expression.toString());
+		config.setPersistent(false);
+		
+		this.timerService.createCalendarTimer(expression, config);
+		
+		System.out.println("Agendamento: " + expression);
+	}
+	
 
 }
